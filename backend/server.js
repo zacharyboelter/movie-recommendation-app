de79d6b43c229c8ed5ec36095136ea8df3683e71
Zachary Boelter <zboelter@gmail.com>
2023-08-29 14:58:34 -0700

    fixed server issue

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
