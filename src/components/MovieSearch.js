5f99f7f52c498bad34896c57645db45813c6b4c9
Zachary Boelter <zboelter@gmail.com>
2023-08-30 15:25:52 -0700

    new component for movie search

---
+++ src/components/MovieSearch.js
@@ -0,0 +1,9 @@
+import React, {useState} from "react";
+import axios from 'axios'
+
+const MovieSearch = () => {
+    const [query, setQuery] = useState('')
+    const [movieData, setMovieData] = useState(null)
+
+    
+}
