56a54434db93d8bb3c5b72f83efdd52844b6e213
Zachary Boelter <zboelter@gmail.com>
2023-08-31 16:00:00 -0700

    import main to App.js for rendering

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 // import Movielist from "./components/movieList";
-import MovieSearch from "./components/MovieSearch";
+// import MovieSearch from "./components/MovieSearch";
+import Main from "./components/Main";
 
 
 const App = () => {
   return (
     <div>
-      <h1>Movie Recommendation App</h1>
-      <MovieSearch />
+      <h1>Movies For Muna</h1>
+      <Main />
     </div>
   )
 }
