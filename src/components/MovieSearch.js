f971bb52b580d8789d693e2923d66fc065489a86
Zachary Boelter <zboelter@gmail.com>
2023-08-31 16:59:59 -0700

    fuck material-ui. installed tailwind and did basic styling on the movie search.

--- src/components/MovieSearch.js
+++ src/components/MovieSearch.js
@@ -1,25 +1,7 @@
 import React, {useState} from "react";
-import { Button, TextField } from "@mui/material";
-import { makeStyles } from '@mui/material';
 
-const useStyles = makeStyles((theme) => ({
-    searchContainer: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        padding: theme.spacing(2),
-        backgroundColor: theme.palette.primary.main,
-        borderRadius: theme.shape.borderRadius,
-        marginBottom: theme.spacing(2),
-    },
-    input: {
-        width: '100%',
-        marginBottom: theme.spacing(1),
-    },
-    button: {
-        width: '100%',
-    },
-}));
+
+
 
 const MovieSearch = ({ onSearch }) => {
     const [query, setQuery] = useState('')
@@ -33,22 +15,25 @@ const MovieSearch = ({ onSearch }) => {
       };
 
     return (
-        <div className={classes.searchContainer}>
-            <h2>Search for a Movie</h2>
-            <form onSubmit={handleSubmit}>
-                <TextField 
-                    className={classes.input}
-                    variant="outlines"
+        <div className="text-center p-4">
+            <h2 className="text-2x1 font-semibold mb-4">Search for a Movie</h2>
+            <form class="flex flex-col items-center mb-2" onSubmit={handleSubmit}>
+                <input
+                    type="text"
                     value={query}
                     onChange={(e) => setQuery(e.target.value)}
                     placeholder="Enter a movie title"
+                    class="px-2 py-1 border rounded mb-2"
                 />
-                <Button className={classes.button}type="submit" variant="contained" color="secondary">
+                <button 
+                    type="submit"
+                    class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+                >
                     Search
-                </Button>
+                </button>
             </form>
         </div>
     );
 }
 
-export default MovieSearch
+export default MovieSearch;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+
+module.exports = {
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ]
+}
+
