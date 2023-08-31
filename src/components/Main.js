632f3f1492f5945d5b935154b7a3cdf9ae7a68d3
Zachary Boelter <zboelter@gmail.com>
2023-08-31 15:52:05 -0700

    main component refactored from movieSeach

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,32 @@
 
+import React, {useState} from "react"
+import MovieSearch from "./MovieSearch"
+import MovieDetails from "./MovieDetails"
+import axios from 'axios'
 
 
+function Main() {
+    const [movieData, setMovieData] = useState(null)
 
-
-const fetchMovieData = async (title) => {
-    // event.preventDefault()
-    console.log('fetchMovieData called')
-    try {
-        const response = await axios.get(`/api/movies/${encodeURIComponent(title)}`)
-        console.log('API request:', response.data)
-        setMovieData(response.data)
-    } catch (error) {
-        console.error('Error fetching data:', error)
+    const fetchMovieData = async (title) => {
+        // event.preventDefault()
+        console.log('fetchMovieData called')
+        try {
+            const response = await axios.get(`/api/movies/${encodeURIComponent(title)}`)
+            console.log('API request:', response.data)
+            setMovieData(response.data)
+        } catch (error) {
+            console.error('Error fetching data:', error)
+        }
     }
+
+    return (
+        <div>
+            <MovieSearch onSearch={fetchMovieData} />
+            {movieData && <MovieDetails movieData={movieData} />}
+        </div>
+    )
 }
+
+
+export default Main
